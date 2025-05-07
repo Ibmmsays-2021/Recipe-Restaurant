c90c0529ad2581881194070fb0b6369c83f9f9fd
ibrahim.mohammed <ibrahim.mohammed@yaquotitech.com>
2025-05-07 11:40:10 +0300

    hellow world

--- src/app/recipes/recipes.component.spec.ts
+++ src/app/recipes/recipes.component.spec.ts
@@ -1,49 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecipesComponent } from './recipes.component';
-import { RecipeService } from './recipe.services';
-import { Recipe } from './recipe.model';
-import { of } from 'rxjs';
-import { EventEmitter } from '@angular/core';
 
 describe('RecipesComponent', () => {
   let component: RecipesComponent;
   let fixture: ComponentFixture<RecipesComponent>;
-  let mockRecipeService: Partial<RecipeService>;
-  let recipeSelectedEmitter: EventEmitter<Recipe>;
-  const mockRecipe: Recipe = {
-    name: 'Test Recipe',
-    description: 'Test Description',
-    imagePath: 'test-image-path.jpg',
-    ingredients: [
-      { name: 'Ingredient 1', amount: 1 },
-      { name: 'Ingredient 2', amount: 2 }
-    ]
-  };
 
   beforeEach(async () => {
-    recipeSelectedEmitter = new EventEmitter<Recipe>();
-    // Create a stub (not a Spy) since recipeSelected is likely an Observable property
-    mockRecipeService = {
-      recipeSelected: recipeSelectedEmitter
-    };
-
     await TestBed.configureTestingModule({
-      declarations: [RecipesComponent],
-      providers: [{ provide: RecipeService, useValue: mockRecipeService }]
+      declarations: [RecipesComponent]
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(RecipesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set selectedRecipe when recipeSelected emits a value', () => {
-    expect(component.selectedRecipe).toEqual(mockRecipe);
+  it('should render "Hello World"', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello World');
   });
 });
