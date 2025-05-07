6eb12809767e09fe4c459c4a896d7e8668132573
ibrahim.mohammed <ibrahim.mohammed@yaquotitech.com>
2025-05-07 11:36:29 +0300

    test

--- src/app/recipes/recipes.component.spec.ts
+++ src/app/recipes/recipes.component.spec.ts
@@ -3,41 +3,47 @@ import { RecipesComponent } from './recipes.component';
 import { RecipeService } from './recipe.services';
 import { Recipe } from './recipe.model';
 import { of } from 'rxjs';
+import { EventEmitter } from '@angular/core';
 
 describe('RecipesComponent', () => {
-    let component: RecipesComponent;
-    let fixture: ComponentFixture<RecipesComponent>;
-    let mockRecipeService: jasmine.SpyObj<RecipeService>;
+  let component: RecipesComponent;
+  let fixture: ComponentFixture<RecipesComponent>;
+  let mockRecipeService: Partial<RecipeService>;
+  let recipeSelectedEmitter: EventEmitter<Recipe>;
+  const mockRecipe: Recipe = {
+    name: 'Test Recipe',
+    description: 'Test Description',
+    imagePath: 'test-image-path.jpg',
+    ingredients: [
+      { name: 'Ingredient 1', amount: 1 },
+      { name: 'Ingredient 2', amount: 2 }
+    ]
+  };
 
-    beforeEach(async () => {
-        mockRecipeService = jasmine.createSpyObj('RecipeService', ['recipeSelected']);
-        mockRecipeService.recipeSelected = of({
-            name: 'Test Recipe',
-            description: 'Test Description',
-            imagePath: 'test-image-path.jpg'
-        } as Recipe);
+  beforeEach(async () => {
+    recipeSelectedEmitter = new EventEmitter<Recipe>();
+    // Create a stub (not a Spy) since recipeSelected is likely an Observable property
+    mockRecipeService = {
+      recipeSelected: recipeSelectedEmitter
+    };
 
-        await TestBed.configureTestingModule({
-            declarations: [RecipesComponent],
-            providers: [{ provide: RecipeService, useValue: mockRecipeService }]
-        }).compileComponents();
-    });
+    await TestBed.configureTestingModule({
+      declarations: [RecipesComponent],
+      providers: [{ provide: RecipeService, useValue: mockRecipeService }]
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(RecipesComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should set selectedRecipe when recipeSelected emits a value', () => {
-        expect(component.selectedRecipe).toEqual({
-            name: 'Test Recipe',
-            description: 'Test Description',
-            imagePath: 'test-image-path.jpg'
-        } as Recipe);
-    });
+  it('should set selectedRecipe when recipeSelected emits a value', () => {
+    expect(component.selectedRecipe).toEqual(mockRecipe);
+  });
 });
